768bea7c70021c1806dbdaac437ddc6e624a4af1
covy <junni802@naver.com>
2025-06-12 23:45:31 +0900

    feat: add socket-alarm

---
+++ src/main/java/covy/socketalarm/SocketAlarmApplication.java
@@ -0,0 +1,13 @@
+package covy.socketalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketAlarmApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SocketAlarmApplication.class, args);
+  }
+
+}
---
+++ src/test/java/covy/socketalarm/SocketAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.socketalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketAlarmApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
